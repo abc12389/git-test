3c2654d920c4e949df35caf68cd642ecf21f4520
ln <ln@qq.com>
2022-11-09 16:29:20 +0800

    hot-fix commit02

--- src/main/java/com/guigu/test01.java
+++ src/main/java/com/guigu/test01.java
@@ -6,6 +6,6 @@ public class test01 {
         System.out.println("22222");
         System.out.println("33333");
         System.out.println("44444");
-        System.out.println("55555");
+        System.out.println("ttttt");
     }
 }
