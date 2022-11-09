62c0dc6a47f9bf215ae1961c934681ac4beadc08
ln <ln@qq.com>
2022-11-09 17:12:17 +0800

    third commit

--- src/main/java/com/guigu/test01.java
+++ src/main/java/com/guigu/test01.java
@@ -7,5 +7,6 @@ public class test01 {
         System.out.println("33333");
         System.out.println("44444");
         System.out.println("ttttt");
+        System.out.println("66666");
     }
 }
